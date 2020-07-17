8b3d86f5d17e2ddf0bcbb029676e467e21f02ea7
Kaporos <theo@daron.be>
2020-07-17 12:47:39 +0200

    Adding base code

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask ,send_file, request # pip install flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    print(request.headers)
+    ip_address = request.remote_addr
+    print("COMMING FROM : "+ip_address)
+    return send_file("pixel.png", mimetype='image/png')
+
+
+if __name__ == "__main__":
+    app.run()
